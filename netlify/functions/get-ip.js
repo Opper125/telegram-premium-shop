51c767fece09d2a1d244b6c5c51335ef3b3f9f65
Opper125 <crakb68@gmail.com>
2025-03-08 02:26:45 +0630

    Delete netlify/functions/get-ip.js

--- netlify/functions/get-ip.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event, context) => {
-    const ip = event.headers['client-ip'] || event.headers['x-forwarded-for'] || '127.0.0.1';
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ ip })
-    };
-};
