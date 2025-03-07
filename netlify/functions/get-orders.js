4e2b9e197d948a00ed1531ace130fa12f7810a6c
Opper125 <crakb68@gmail.com>
2025-03-08 02:27:50 +0630

    Update get-orders.js

--- netlify/functions/get-orders.js
+++ netlify/functions/get-orders.js
@@ -2,8 +2,9 @@ const fs = require('fs');
 const path = require('path');
 
 exports.handler = async (event, context) => {
-    const orders = fs.existsSync(path.join(__dirname, 'orders.json'))
-        ? JSON.parse(fs.readFileSync(path.join(__dirname, 'orders.json')))
+    const filePath = path.join(__dirname, 'orders.json');
+    const orders = fs.existsSync(filePath)
+        ? JSON.parse(fs.readFileSync(filePath))
         : [];
     return {
         statusCode: 200,
