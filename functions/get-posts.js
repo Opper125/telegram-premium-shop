0c412b01392703663b1e3c814cb1f9f5634160a4
Opper125 <crakb68@gmail.com>
2025-03-10 04:47:09 +0630

    Delete functions directory

--- functions/get-posts.js
+++
@@ -1,8 +0,0 @@
-const EventEmitter = require('events');
-const postEmitter = new EventEmitter();
-let posts = [];
-
-exports.handler = async (event, context) => {
-    if (event.httpMethod !== 'GET') return { statusCode: 405, body: 'Method not allowed' };
-    return { statusCode: 200, body: JSON.stringify(posts) };
-};
